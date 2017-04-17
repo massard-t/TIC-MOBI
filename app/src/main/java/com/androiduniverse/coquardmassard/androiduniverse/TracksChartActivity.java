a6e6b5b085b1252e10f04766740a5685cb5f9ba9
alexiscoquard <coquar_a@etna-alternance.net>
2017-04-17 16:37:49 +0200

    MVP shit

--- app/src/main/java/com/androiduniverse/coquardmassard/androiduniverse/TracksChartActivity.java
+++ app/src/main/java/com/androiduniverse/coquardmassard/androiduniverse/TracksChartActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.view.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,41 +14,25 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class TracksChartActivity extends AppCompatActivity {
+public class TracksChartActivity extends AppCompatActivity implements TracksChartPresenter.View {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private final static String API_KEY = "frZWMPA11XchelzfulMkqCVZEaBFmE67eFrkgGfPYzgyIvckXts";
+
 
     public ListView tracksChartView;
     List<String> tracks = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        TracksChartPresenter presenter = new TracksChartPresenter(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracks_chart);
-
         tracksChartView = (ListView) findViewById(R.id.TracksListView);
+        presenter.askAPI();
+    }
 
-        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
-        Call<TracksChart> call = apiService.getTopRatedTracks(API_KEY);
-        call.enqueue(new Callback<TracksChart>() {
-            public void onResponse(Call<TracksChart>call, Response<TracksChart> response) {
-                TracksChart trackschart = new TracksChart(response.body().tracks);
-
-                for (int i = 0 ; i < trackschart.tracks.size() ; i++) {
-                    tracks.add(trackschart.tracks.get(i).getTitle());
-                }
-
-                Log.d(TAG, "" + trackschart.tracks.size());
-
-                //Log.d(TAG, "Number of movies received: " + tracks.size());
-            }
-
-            public void onFailure(Call<TracksChart>call, Throwable t) {
-                // Log error here since request failed
-                Log.e(TAG, t.toString());
-            }
-        });
+    public void updateTracklist(List<String> tracks) {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, tracks);
         tracksChartView.setAdapter(adapter);
--- app/src/main/java/com/androiduniverse/coquardmassard/androiduniverse/TracksChartPresenter.java
+++ app/src/main/java/com/androiduniverse/coquardmassard/androiduniverse/TracksChartPresenter.java
@@ -1,12 +1,14 @@
 package com.androiduniverse.coquardmassard.androiduniverse;
 
 import android.util.Log;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by alexiscoquard on 17/04/2017.
@@ -14,4 +16,43 @@ import retrofit2.Callback;
 
 public class TracksChartPresenter {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private final static String API_KEY = "frZWMPA11XchelzfulMkqCVZEaBFmE67eFrkgGfPYzgyIvckXts";
+
+    private List<String> tracks = new ArrayList<String>();
+    private TracksChart trackschart;
+
+    View view;
+
+    public TracksChartPresenter(View view) {
+        this.view = view;
+    }
+
+    public void askAPI() {
+        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        Call<TracksChart> call = apiService.getTopRatedTracks(API_KEY);
+        call.enqueue(new Callback<TracksChart>() {
+            public void onResponse(Call<TracksChart>call, Response<TracksChart> response) {
+                trackschart = new TracksChart(response.body().tracks);
+
+                for (int i = 0 ; i < trackschart.tracks.size() ; i++) {
+                    tracks.add(trackschart.tracks.get(i).getTitle());
+                }
+
+                Log.d(TAG, "" + trackschart.tracks.size());
+
+                //Log.d(TAG, "Number of movies received: " + tracks.size());
+            }
+
+            public void onFailure(Call<TracksChart>call, Throwable t) {
+                // Log error here since request failed
+                Log.e(TAG, t.toString());
+            }
+        });
+        view.updateTracklist(tracks);
+    }
+
+    public interface View {
+        void updateTracklist(List<String> tracks);
+    }
 }
