21248e4ac45ebdfbc64668a28acfe5fc411453c6
Theo Massard <massar_t@etna-alternance.net>
2017-09-30 15:16:25 +0200

    Token doesn't work anymore, just putting a placeholder for it

--- app/src/main/java/com/androiduniverse/coquardmassard/androiduniverse/ApiClient.java
+++ app/src/main/java/com/androiduniverse/coquardmassard/androiduniverse/ApiClient.java
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 class ApiClient {
     private static final String BASE_URL = "http://api.deezer.com/";
     private static Retrofit retrofit = null;
-    private static final String API_TOKEN = "frMu6zNpTUPfZLCffb2TXNg1TzJcPabp1SPOIN77NJP4eAqj2Ko";
+    private static final String API_TOKEN = "YOUR TOKEN";
 
 
     static Retrofit getClient() {
